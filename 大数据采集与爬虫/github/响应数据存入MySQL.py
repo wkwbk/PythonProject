86c53e1cfe4d1d0aacd8c994650720ff8378d39c
wkwbk <lisir.me@outlook.com>
2023-04-02 09:16:15 +0800

    代码更新

--- 大数据采集与爬虫/github/响应数据存入MySQL.py
+++ 大数据采集与爬虫/github/响应数据存入MySQL.py
@@ -3,7 +3,8 @@ import pymysql
 
 search_content = "spider"
 api_url = f"https://api.github.com/search/repositories?q={search_content}"
-ua = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69"}
+ua = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69"}
 
 print("--------------------------------- GET 请求 ---------------------------------")
 
@@ -18,11 +19,6 @@ req_dic_items = req_dic["items"]
 print("当前页面返回的项目数量：", len(req_dic_items))
 
 print("--------------------------------- 创建 github 数据库 ---------------------------------")
-
-# db1 = pymysql.connect(host="localhost", user="root", password="778057151", port=3306)
-# cursor1 = db1.cursor()
-# cursor1.execute("CREATE DATABASE github DEFAULT CHARACTER SET utf8mb4")
-# db1.close()
 
 db1 = pymysql.connect(host="localhost", user="root", password="778057151", port=3306)
 cursor1 = db1.cursor()
@@ -40,23 +36,41 @@ print("--------------------------------- 创建 webapi 表 ---------------------
 db2 = pymysql.connect(host="localhost", user="root", password="778057151", port=3306, database="github")
 cursor2 = db2.cursor()
 cursor2.execute("DROP TABLE IF EXISTS webapi")
+
 sql1 = """CREATE TABLE webapi(
 index_id int(10) NOT NULL AUTO_INCREMENT,
 github_id int(10) NOT NULL,
 full_name char(100) NOT NULL,
 html_url char(100) NOT NULL,
+forks_count int(10) NOT NULL,
+watchers_count int(10) NOT NULL,
 PRIMARY KEY (index_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"""
+
 cursor2.execute(sql1)
 print("创建表成功")
 
 print("--------------------------------- 写入数据 ---------------------------------")
 
 for index_id, key in enumerate(req_dic_items, start=1):
-    print("项目序号:", index_id, "|", "项目 ID:", key["id"], "|", "项目名称:", key["full_name"], "|", "项目链接:", key["html_url"])
-    sql2 = "INSERT INTO webapi(index_id, github_id, full_name, html_url) VALUES(%s, %s, %s, %s)"
+    print(
+        "项目序号:", index_id, "|",
+        "项目 ID:", key["id"], "|",
+        "项目名称:", key["full_name"], "|",
+        "项目链接:", key["html_url"], "|",
+        "项目分支数:", key["forks"], "|",
+        "项目星标数:", key["watchers"])
+
+    sql2 = """INSERT INTO webapi(
+    index_id,
+    github_id,
+    full_name,
+    html_url,
+    forks_count,
+    watchers_count) VALUES(%s, %s, %s, %s, %s, %s)"""
+
     try:
-        cursor2.execute(sql2, (index_id, key["id"], key["full_name"], key["html_url"]))
+        cursor2.execute(sql2, (index_id, key["id"], key["full_name"], key["html_url"], key["forks"], key["watchers"]))
         db2.commit()
     except:
         print("Error")
