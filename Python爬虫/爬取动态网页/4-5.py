e6d2af28ff06b475158f809e1b118aa944c5be34
wkwbk <lisir.me@outlook.com>
2023-04-14 09:15:48 +0800

    代码更新

--- Python爬虫/爬取动态网页/4-5.py
+++ Python爬虫/爬取动态网页/4-5.py
@@ -6,5 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get("https://www.ptpress.com.cn/search/books")
 wait = WebDriverWait(driver, 10)
-confirm_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#app > div:nth-child(1) > div > div > div > button > i")))
+confirm_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#app > div:nth-child(1) > div > div > div.search_main.down_search > button > i')))
 confirm_btn.click()
+
+driver.get("https://www.ptpress.com.cn/search/books")
+driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+driver.execute_script('alert("Python 爬虫")')
